710600cefd6064f0eef83cb59debc20677e224d9
Viktor Malmedal <38523983+johnvicke@users.noreply.github.com>
2025-11-22 08:31:35 +0100

    add doppler

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
-
+  
   outputs = { nixpkgs, ... }:
     let
       forAllSystems = function:
@@ -10,23 +10,53 @@
           (system:
             let
               pkgs = import nixpkgs {
-                system = system;
+                inherit system;
+                overlays = [
+                  (final: prev: {
+                    doppler = prev.stdenv.mkDerivation rec {
+                      pname = "doppler";
+                      version = "3.75.1";
+                      
+                      src = prev.fetchurl {
+                        url = "https://github.com/DopplerHQ/cli/releases/download/${version}/doppler_${version}_linux_amd64.tar.gz";
+                        sha256 = "sha256-C4WCMtqpo/0G0cPxuaNwxoz+P5aA7Bpg1UAZnNhZvqA=";
+                      };
+                      
+                      sourceRoot = ".";
+                      
+                      installPhase = ''
+                        runHook preInstall
+                        
+                        mkdir -p $out/bin
+                        install -m755 doppler $out/bin/doppler
+                        
+                        runHook postInstall
+                      '';
+                      
+                      meta = with prev.lib; {
+                        description = "The official CLI for interacting with your Doppler secrets";
+                        homepage = "https://doppler.com";
+                        license = licenses.asl20;
+                        platforms = [ "x86_64-linux" ];
+                      };
+                    };
+                  })
+                ];
               };
             in function pkgs
           );
     in {
       formatter = forAllSystems (pkgs: pkgs.alejandra);
-
+      
       devShells = forAllSystems (pkgs: {
         default = pkgs.mkShell {
           packages = with pkgs; [
-						flyctl
+            flyctl
             corepack
             bun
+            doppler
           ];
         };
       });
     };
 }
-
-
