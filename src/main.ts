3535e414eeb3f5f3f6743b0e9fcda080df60f081
Viktor Malmedal <38523983+johnvicke@users.noreply.github.com>
2025-11-24 17:23:38 +0100

    update main

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,18 @@
 import { HttpLayerRouter, HttpServerResponse, Socket } from "@effect/platform";
 import { BunHttpServer, BunRuntime } from "@effect/platform-bun";
 import { Effect, Layer, Logger } from "effect";
-import { BotLayerLive } from "./pkgs/dicord/bot-layer";
-import { DiscordEventCoordinator } from "./pkgs/dicord/coordinator";
+import { BotLayerLive } from "./pkgs/discord/bot-layer";
+import { DiscordEventCoordinator } from "./pkgs/discord/coordinator";
 import {
 	type Command,
 	type CommandContext,
 	DiscordGateway,
-} from "./pkgs/dicord/gateway";
-import { DiscordHttp } from "./pkgs/dicord/http";
-import { DiscordCommandRegistry } from "./pkgs/dicord/registry";
-import type { SlashCommand } from "./pkgs/dicord/schemas";
+} from "./pkgs/discord/gateway";
+import { DiscordHttp } from "./pkgs/discord/http";
+import { DiscordCommandRegistry } from "./pkgs/discord/registry";
+import type { SlashCommand } from "./pkgs/discord/schemas";
 import { Otel } from "./pkgs/otel";
+import { PubgApi } from "./pkgs/pubg/http";
 
 export const defineCommand = <A>(
 	schema: SlashCommand,
@@ -29,24 +30,40 @@ const pingCommand = defineCommand(
 	(ctx) => ctx.respond("Pong!"),
 );
 
+const statsCommand = defineCommand(
+	{
+		name: "stats",
+		description: "Replies with Pong!",
+	},
+	(ctx) => ctx.respond("Pong!"),
+);
+
 const Main = Layer.scopedDiscard(
 	Effect.gen(function* () {
 		const http = yield* DiscordHttp;
 		const gateway = yield* DiscordGateway;
 		const registry = yield* DiscordCommandRegistry;
 		const coordinator = yield* DiscordEventCoordinator;
+		const pubg = yield* PubgApi;
+
+		const player = yield* pubg.findPlayerByName("Nandrolone");
+
+		if (player._tag === "Some") {
+			console.log(player.value);
+			const stats = yield* pubg.getSeasonStats(player.value.id);
+			console.log(stats);
+		}
 
 		yield* registry.register(pingCommand);
+		yield* registry.register(statsCommand);
 		yield* registry.syncWithDiscord;
 
 		const websocketUrl = yield* http.getWssUrl();
-		const url = new URL(websocketUrl)
-		url.searchParams.set("v", "10")
-		url.searchParams.set("encoding", "json")
+		const url = new URL(websocketUrl);
+		url.searchParams.set("v", "10");
+		url.searchParams.set("encoding", "json");
 
-		yield* Effect.forkDaemon(
-			gateway.connect(url.toString()),
-		);
+		yield* Effect.forkDaemon(gateway.connect(url.toString()));
 		yield* Effect.forkDaemon(coordinator.start);
 	}),
 );
@@ -68,6 +85,7 @@ const HttpLive = HttpLayerRouter.serve(
 
 const AppLayer = Layer.mergeAll(Main, HttpLive).pipe(
 	Layer.provide(BotLayerLive),
+	Layer.provide(PubgApi.Default),
 	Layer.provide(Socket.layerWebSocketConstructorGlobal),
 	Layer.provide([Otel.layer, Logger.structured]),
 );
