491e7e70f2173135ec8e5c64db82ca2b7fc4dd68
Viktor Malmedal <38523983+johnvicke@users.noreply.github.com>
2025-11-24 17:23:35 +0100

    add docstring

--- src/pkgs/schema/tagged-struct.ts
+++ src/pkgs/schema/tagged-struct.ts
@@ -1,5 +1,11 @@
 import { Schema, type SchemaAST } from "effect";
 
+/**
+ * @description
+ * Schema.TaggedStruct does not automatically add a `_tag` field
+ * during decoding, so this utility function creates a tagged struct schema
+ * with a `_tag` field that is set to the provided tag value.
+ */
 export const TaggedStruct = <
 	Tag extends SchemaAST.LiteralValue,
 	Fields extends Schema.Struct.Fields,
